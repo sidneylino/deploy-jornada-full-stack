4edb9d2b7e2c7f9e685cbee09d68caaa3ddf7ae6
sidneylino <sidneymartinslino@gmail.com>
2025-02-16 16:50:35 -0300

    ajustes deploy

--- api-spotify-project/api/server.js
+++ api-spotify-project/api/server.js
@@ -1,26 +1,33 @@
-// Middleware
-
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 
-app.get('/', (request, response) => {
+app.get('/api/', (request, response) => {
    response.send('ola,mundosss') 
 });
 
-app.get('/artists', async (request, response) => {
+app.get('/api/artists', async (request, response) => {
     response.send(await db.collection('artists').find({}).toArray());
 });
 
-app.get('/songs', async (request, response) => {
+app.get('/api/songs', async (request, response) => {
    response.send(await db.collection('songs').find({}).toArray());
 }); 
 
+app.use(express.static(path.join(__dirname,"../project-spotify/dist")));
+
+app.get('*', async (request, response) => {
+    response.sendFile(path.join(__dirname,"../project-spotify/dist/index.html"));
+ }); 
+ 
 app.listen(PORT, () => {
     console.log(`servidor está escutando na porta ${PORT}`)
 });
---
+++ api-spotify-project/api/test.js
@@ -0,0 +1,4 @@
+import path, { dirname } from "path";
+
+const __dirname = path.resolve();
+console.log(__dirname)
